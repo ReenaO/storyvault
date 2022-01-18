c3acbc0875c88c4f28e2051279b3fec97aaf4cca
ReenaO <reena.mccarthy@hotmail.com>
2022-01-18 19:00:17 +0000

    welcome picture and comment on classes

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
+1. welcome
+2. cinema
+3. zoo
+4. flower
+5. chocolate
+6. kite
+7. goodbye
+
 noun = ['Mountain', 'Ocean', 'Building', 'Playground', 'Animal']
 adjectives = ['Running', 'Walking', 'Cycling', 'Sleeping', 'Eating']
 people = ['Mother', 'Father', 'Sister', 'Brother', 'Friend', 'Husband']
